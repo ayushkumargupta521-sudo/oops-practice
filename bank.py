2c73cef4393f610fb2320c1442c8b5a3749d31c2
Ayush Kumar Gupta <ayushkumargupta521@gmail.com>
2026-09-09 19:29:02 +0530

    Banking Atm Sytem

---
+++ bank.py
@@ -0,0 +1,17 @@
+class ATM:
+    def __int__(customer):
+        customer.name = ""
+        customer.pin = ""
+        customer.deposit = ""
+        customer.withdraw = ""
+        customer.totalamount = ""
+
+    def bank(customer):
+        user_input = ("""
+        1. Enter Your Name
+        2. Set Pin
+        3. Deposit
+        4. Withdraw
+        5. Total Amount
+        6. Exit
+        """)
