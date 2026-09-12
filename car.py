1d18899d6de6f2cccdd5b7974890b261aa8dd3ec
Ayush Kumar Gupta <ayushkumargupta521@gmail.com>
2026-09-12 12:45:05 +0530

    Different Types of Cars

--- car.py
+++ car.py
@@ -1,22 +1,38 @@
 class Car:
-    def __init__(self,brand,model):
-        self.brand = brand
+
+    total_car = 0
+
+    def __init__(self, brand, model):
+        self.__brand = brand
         self.model = model
+        Car.total_car += 1
 
     def fullname(self):
-        return f"{self.brand} {self.model}"
+        return f"{self.__brand} {self.model}"
+
+    def get_brand(self):
+        return self.__brand
+
+    def fuel_type(self):
+        return "Petro or Diesel"
+
 
 class ElectricCar(Car):
-    def __init__(self,brand,model,battery):
-        super().__init__(brand,model)
+
+    def __init__(self, brand, model, battery):
+        super().__init__(brand, model)
         self.battery = battery
 
+    def fuel_type(self):
+        return "Electric Charge"
 
-my_car = ElectricCar("Tesla","S","80Kh")
-print(my_car.fullname())
 
+my_car = ElectricCar("Tesla", "S", "80Kh")
+print(my_car.fuel_type())
 
-# my_car = Car("Rollce Royce","BM")
-# print(my_car.brand)
-# print(my_car.model)
-# print(my_car.fullname())
+car = ElectricCar("Tesla", "M", "90")
+
+my_car = Car("Rollce Royce", "BM")
+print(my_car.fuel_type())
+
+print(Car.total_car)
